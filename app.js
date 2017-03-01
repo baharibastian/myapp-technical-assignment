6377ee0173b4a8ce1ca6034a53073e227fca3b0f
baharibastian <bastian17.98@gmail.com>
2017-03-01 23:03:50 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express()
+
+app.set('view engine', 'pug')
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Bahari Bastian-Technical Assignment', message: 'Hello there!' })
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
